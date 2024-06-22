37fbffdb051b091aa9e51cdfeda72ca438420e37
Ziyodullo <ziyodillo808@gmail.com>
2024-06-22 16:36:02 +0300

    unit test for getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -19,4 +20,33 @@ describe('MoviesService', () => {
   it("should be 0", () => {
     expect(5-4).toEqual(1);
   })
+
+  describe('testing getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('testing getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2000,
+        genres: ['test'],
+      })
+      const result = service.getOne(1);
+      expect(result).toBeDefined();
+      // expect(result.id).toEqual(1);
+    });
+
+    it('should return 404 page', () => {
+      try{
+        service.getOne(89)
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException)
+        // expect(e.message).toEqual("Movie with ID 89 not found.")
+      }
+    })
+  });
 });
